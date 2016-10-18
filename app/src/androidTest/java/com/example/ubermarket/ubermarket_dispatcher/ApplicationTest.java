2d2bc5d3c66b43f84b9873a44e8c40630a783a44
Miguel Maffet <Miguel@MacBook-Pro-de-Miguel-2.local>
2016-10-18 14:49:28 -0300

    Initial Commit

---
+++ app/src/androidTest/java/com/example/ubermarket/ubermarket_dispatcher/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.ubermarket.ubermarket_dispatcher;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
